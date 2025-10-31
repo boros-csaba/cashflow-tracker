bd770bc421cba038e147ae933883c1f0c7378f50
Csaba Boros <boros.csaba94@gmail.com>
2025-10-31 12:43:51 +0100

    fix

--- CashflowTracker/Program.cs
+++ CashflowTracker/Program.cs
@@ -1,29 +1,8 @@
 ﻿using CashflowTracker.Services;
-using System.Text;
 
 string dataFolder = "../../../../data";
 var transactions = await TransactionsCsvProcessor.ProcessFiles(dataFolder);
+transactions = transactions.OrderByDescending(t => t.Date).ToList();
 
-// Sort transactions by date descending
-var sortedTransactions = transactions.OrderByDescending(t => t.Date).ToList();
+Console.WriteLine($"Total transactions loaded: {transactions.Count}");
 
-Console.WriteLine($"Total transactions loaded: {sortedTransactions.Count}");
-
-// Export to CSV
-string outputPath = "C:/Users/boros/My Drive/transactions.csv";
-Directory.CreateDirectory(Path.GetDirectoryName(outputPath)!);
-
-using (var writer = new StreamWriter(outputPath, false, Encoding.UTF8))
-{
-    // Write header
-    writer.WriteLine("ID,Date,Type,Recipient,Amount,Currency,Description,Source");
-
-    // Write transactions
-    foreach (var transaction in sortedTransactions)
-    {
-        var csvLine = $"\"{transaction.Id}\",\"{transaction.Date:yyyy-MM-dd}\",\"{transaction.Type}\",\"{transaction.Recipient}\",{transaction.Amount},\"{transaction.Currency}\",\"{transaction.AdditionalInfo}\",\"{transaction.Source}\"";
-        writer.WriteLine(csvLine);
-    }
-}
-
-Console.WriteLine($"Exported {sortedTransactions.Count} transactions to {outputPath}");
